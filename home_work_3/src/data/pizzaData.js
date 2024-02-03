fafb4817b251a80cd90e41e792367e0538f131d7
ShkAlexO <shk.alex.o@gmail.com>
2024-02-03 14:00:50 +0200

    Add home_work_3

---
+++ home_work_3/src/data/pizzaData.js
@@ -0,0 +1,95 @@
+const pizzaData = [
+    {
+        id: '1',
+        title: 'Pizza capricciosa',
+        description: 'Italian pizza made with baked ham, mushrooms.',
+        priceNew: '10',
+        oldPrice: '17',
+        isNew: true, 
+        image: require('../../assets/pizza.png')
+    },
+    {
+      id: '2',
+      title: 'Pepperoni',
+      description: 'Pizza with spicy pepperoni, tomato sauce, and mozzarella cheese.',
+      priceNew: '14',
+      oldPrice: '18',
+      isNew: false,
+      image: require('../../assets/pizza.png')
+    },
+    {
+      id: '3',
+      title: 'Vegetarian',
+      description: 'Vegetarian pizza with fresh vegetables and cheese.',
+      priceNew: '13',
+      oldPrice: '16',
+      isNew: true,
+      image: require('../../assets/pizza.png')
+    },
+    {
+      id: '4',
+      title: 'BBQ Chicken',
+      description: 'Pizza with barbecue chicken, red onions, and mozzarella cheese.',
+      priceNew: '16',
+      oldPrice: '20',
+      isNew: false,
+      image: require('../../assets/pizza.png')
+    },
+    {
+      id: '5',
+      title: 'Hawaiian',
+      description: 'Hawaiian pizza with ham, pineapple, and mozzarella cheese.',
+      priceNew: '15',
+      oldPrice: '19',
+      isNew: true,
+      image: require('../../assets/pizza.png')
+    },
+    {
+      id: '6',
+      title: 'Supreme',
+      description: 'Supreme pizza with pepperoni, sausage, bell peppers, and olives.',
+      priceNew: '18',
+      oldPrice: '23',
+      isNew: false,
+      image: require('../../assets/pizza.png')
+    },
+    {
+      id: '7',
+      title: 'Mushroom Delight',
+      description: 'Pizza with a variety of mushrooms and cheese.',
+      priceNew: '14',
+      oldPrice: '17',
+      isNew: true,
+      image: require('../../assets/pizza.png')
+    },
+    {
+      id: '8',
+      title: 'Spinach and Feta',
+      description: 'Pizza with spinach, feta cheese, and tomatoes.',
+      priceNew: '13',
+      oldPrice: '16',
+      isNew: false,
+      image: require('../../assets/pizza.png')
+    },
+    {
+      id: '9',
+      title: 'Meat Lovers',
+      description: 'Pizza for meat lovers with a mix of different meats and cheese.',
+      priceNew: '20',
+      oldPrice: '25',
+      isNew: true,
+      image: require('../../assets/pizza.png')
+    },
+    {
+      id: '10',
+      title: 'Four Cheese',
+      description: 'Pizza with a blend of four cheeses: mozzarella, cheddar, feta, and parmesan.',
+      priceNew: '17',
+      oldPrice: '21',
+      isNew: false,
+      image: require('../../assets/pizza.png')
+    },
+]
+
+
+export default pizzaData;
